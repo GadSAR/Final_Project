38888798881fcbf5da4a8f3212f3448aae3d50dc
GadSAR <sargad123@gmail.com>
2023-04-11 15:53:11 +0300

    3d model improve

--- IFM_IntelliFleetManagement/react_frontend_ifm/src/components/canvas/Car.jsx
+++ IFM_IntelliFleetManagement/react_frontend_ifm/src/components/canvas/Car.jsx
@@ -1,13 +1,19 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useEffect, useMemo, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "./Loader";
 
 const Car = ({ isMobile }) => {
   const car = useGLTF("./night_car_landscape/scene.gltf");
+  const rotationRef = useRef(0);
 
-  return (
+  useFrame(({ clock }) => {
+    rotationRef.current += 0.005;
+    car.scene.rotation.y = rotationRef.current;
+  });
+
+  return useMemo(() => (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
@@ -21,33 +27,26 @@ const Car = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={car.scene}
-        scale={isMobile ? 1.17 : 1.175}
-        position={isMobile ? [0, -3, -20.2] : [0, -3.25, -10.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.05 : 0.05}
       />
     </mesh>
-  );
+  ), [car, isMobile]);
 };
 
 const CarCanva = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
@@ -55,14 +54,16 @@ const CarCanva = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop='always'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [70, 50, 10], fov: 20 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          enablePan={false}
+          enableRotate={true}
           enableZoom={true}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
